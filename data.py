6e01e050cf7c7ddc6c5bd1678ffb2cc7d2ee4c23
Danish Hilman <danishhilmanazizul@gmail.com>
2022-09-16 11:55:36 +0800

    Added data file, created get_all_pokemon_name

---
+++ data.py
@@ -0,0 +1,37 @@
+import csv
+
+pokedex = []
+with open("pokedex.csv", newline="") as pokedex_csv:
+  pokedex_reader = csv.DictReader(pokedex_csv, delimiter=",")
+  for row in pokedex_reader:
+    pokedex.append(row)
+
+def get_all_pokemon_name():
+        pokemon_name = []
+        index = 0
+        for i in pokedex:
+            
+            if i["ï»¿#"] == "":
+                pass
+            
+            elif i["ï»¿#"].startswith("Mega"):
+                pokemon_name.append(pokedex[index + 1]["Name"])
+
+            else:
+                pokemon_name.append(i["Name"])
+            index += 1
+        return pokemon_name
+
+def get_all_pokemon_name_and_type():
+        all_pokemon_names_and_type = {x: None for x in get_all_pokemon_name()}
+        index = 0
+        for i in all_pokemon_names_and_type:
+            all_pokemon_names_and_type[i] = [pokedex[index]["Type"]]
+            if pokedex[index+1]["ï»¿#"] == "":
+                if pokedex[index+1]["Type"] != "":
+                    all_pokemon_names_and_type[i].append(pokedex[index + 1]["Type"])
+                    index += 1
+            index += 1
+        return all_pokemon_names_and_type
+            
+
--- recommendation_program.py
+++ recommendation_program.py
@@ -1,1 +1,3 @@
-print("Hello world")
+from data import get_all_pokemon_name, get_all_pokemon_name_and_type, pokedex
+
+print(get_all_pokemon_name())
