26ad5e29449067b0f693fced57483861b62c7c1f
Danish Hilman <danishhilmanazizul@gmail.com>
2022-10-18 12:35:17 +0800

    Refactored all functions for name, type, stats.

--- recommendation_program.py
+++ recommendation_program.py
@@ -5,30 +5,58 @@ from data import get_all_pokemon_name, get_all_pokemon_name_and_type, get_all_po
 # print(get_all_pokemon_name_and_stats())
 # print(get_all_pokemon_name_type_stats())
 
+#prints all pokemon mapped to their respective type and stats
+def display_all_pokemon():
+
+    all_pokemon = get_all_pokemon_name_type_stats()
+    for i in all_pokemon:
+        print("{pokemon}\n Type: {type}\n Stats: {stats}\n".format(pokemon=i, type=all_pokemon[i]["Type"], stats=all_pokemon[i]["Stats"]))
+
+#if a pokemon name starts with the specified letter/word; prints the pokemon mapped to their respective type and stats
 def pokemon_by_starting_letter(letter):
 
-    for i in sorted(get_all_pokemon_name_and_type()):
+    all_pokemon = get_all_pokemon_name_type_stats()
+    for i in sorted(all_pokemon):
         if i.startswith(str(letter).title()):
-            print(i, get_all_pokemon_name_and_type()[i])
+            print("{pokemon}\n Type: {type}\n Stats: {stats}\n".format(pokemon=i, type=all_pokemon[i]["Type"], stats=all_pokemon[i]["Stats"]))
 
+#prints all pokemon which has the specified type OR prints all pokemon with only the specified type
 def pokemon_with_type(type, only_type=False):
-    
-    for i in get_all_pokemon_name_and_type():
+
+    all_pokemon = get_all_pokemon_name_type_stats()
+    for i in all_pokemon:
+        #prints all pokemon with only the specified
         if only_type == True:
-            if type in get_all_pokemon_name_and_type()[i] and len(get_all_pokemon_name_and_type()[i]) == 1:
-                print(i, get_all_pokemon_name_and_type()[i])
+            if type in all_pokemon[i]["Type"] and len(all_pokemon[i]["Type"]) == 1:
+                print("{pokemon}\n Type: {type}\n Stats: {stats}\n".format(pokemon=i, type=all_pokemon[i]["Type"], stats=all_pokemon[i]["Stats"]))
 
-        elif type in get_all_pokemon_name_and_type()[i]:
-            print(i, get_all_pokemon_name_and_type()[i])
+        #prints all pokemon with the specified type regardless of other type
+        elif type in all_pokemon[i]["Type"]:
+                print("{pokemon}\n Type: {type}\n Stats: {stats}\n".format(pokemon=i, type=all_pokemon[i]["Type"], stats=all_pokemon[i]["Stats"]))
 
+#prints all pokemon with a specified stats category with a maximum value, minimum value or specific value
 def pokemon_with_stats(stat_type, val, min=False, max=False):
+    all_pokemon = get_all_pokemon_name_type_stats()
     pokemon_stats_dict = {}
-    for i in get_all_pokemon_name_and_stats():
-        if get_all_pokemon_name_and_stats()[i][stat_type] >= val:
-            pokemon_stats_dict[i] = get_all_pokemon_name_and_stats()[i][stat_type]
-    sorted_pokemon_stats_dict = dict(sorted(pokemon_stats_dict.items(), key=lambda item: item[1]))
-    print(sorted_pokemon_stats_dict)
+    for i in all_pokemon:
+        if min == True:
+            if all_pokemon[i]["Stats"][stat_type] >= val:
+                pokemon_stats_dict[i] = all_pokemon[i]["Stats"][stat_type]
+        elif max == True: 
+            if all_pokemon[i]["Stats"][stat_type] <= val:
+                pokemon_stats_dict[i] = all_pokemon[i]["Stats"][stat_type]
+        else:
+            if all_pokemon[i]["Stats"][stat_type] == val:
+                pokemon_stats_dict[i] = all_pokemon[i]["Stats"][stat_type]
+
+    pokemon_stats_dict = dict(sorted(pokemon_stats_dict.items(), key=lambda item: item[1]))
+    
+    for i in pokemon_stats_dict:
+        print("{pokemon}\n Type: {type}\n Stats: {stats}\n".format(pokemon=i, type=all_pokemon[i]["Type"], stats=all_pokemon[i]["Stats"]))
+    
 
 # pokemon_by_starting_letter("D")
-# pokemon_with_type("FIRE", True)
-# pokemon_with_stats("Speed", 100)
+# pokemon_with_type("FIRE")
+pokemon_with_stats("Speed", 100, max=True)
+# display_all_pokemon()
+# print(get_all_pokemon_name_type_stats())
