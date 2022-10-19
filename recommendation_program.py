1c0a3fcfec5e3dbe8d190d9e500e2ccf99b16259
Danish Hilman <danishhilmanazizul@gmail.com>
2022-10-19 12:26:25 +0800

    added 2 type sort, add display func,add 2layersort

--- recommendation_program.py
+++ recommendation_program.py
@@ -1,62 +1,63 @@
-from data import get_all_pokemon_name, get_all_pokemon_name_and_type, get_all_pokemon_name_and_stats, get_all_pokemon_name_type_stats, pokedex
-
-# print(get_all_pokemon_name())
-# print(get_all_pokemon_name_and_type())
-# print(get_all_pokemon_name_and_stats())
-# print(get_all_pokemon_name_type_stats())
+from data import pokedex
 
 #prints all pokemon mapped to their respective type and stats
-def display_all_pokemon():
+def display_pokemon_list(pokemon_list=pokedex):
 
-    all_pokemon = get_all_pokemon_name_type_stats()
-    for i in all_pokemon:
-        print("{pokemon}\n Type: {type}\n Stats: {stats}\n".format(pokemon=i, type=all_pokemon[i]["Type"], stats=all_pokemon[i]["Stats"]))
+    for i in pokemon_list:
+        print("{pokemon}\n Type: {type}\n Stats: {stats}\n".format(pokemon=i, type=pokemon_list[i]["Type"], stats=pokemon_list[i]["Stats"]))
 
-#if a pokemon name starts with the specified letter/word; prints the pokemon mapped to their respective type and stats
-def pokemon_by_starting_letter(letter):
+#if a pokemon name starts with the specified letter/word; returns a dictionary with the pokemon mapped to their respective type and stats
+def pokemon_by_starting_letter(letter, pokemon_list=pokedex):
 
-    all_pokemon = get_all_pokemon_name_type_stats()
-    for i in sorted(all_pokemon):
+    sorted_pokemon = {}
+    for i in sorted(pokemon_list):
         if i.startswith(str(letter).title()):
-            print("{pokemon}\n Type: {type}\n Stats: {stats}\n".format(pokemon=i, type=all_pokemon[i]["Type"], stats=all_pokemon[i]["Stats"]))
+            sorted_pokemon[i] = {"Type":pokemon_list[i]["Type"], "Stats":pokemon_list[i]["Stats"]}
+    return sorted_pokemon
 
-#prints all pokemon which has the specified type OR prints all pokemon with only the specified type
-def pokemon_with_type(type, only_type=False):
+#returns a dictionary of all the pokemon which has the specified type OR returns a list of all pokemon with only the specified type
+def pokemon_with_type(*type, only_type=False, pokemon_list=pokedex):
 
-    all_pokemon = get_all_pokemon_name_type_stats()
-    for i in all_pokemon:
-        #prints all pokemon with only the specified
+    sorted_pokemon = {}
+    type = list(type)
+    for i in pokemon_list:
+        #adds all pokemon with only the specified type
         if only_type == True:
-            if type in all_pokemon[i]["Type"] and len(all_pokemon[i]["Type"]) == 1:
-                print("{pokemon}\n Type: {type}\n Stats: {stats}\n".format(pokemon=i, type=all_pokemon[i]["Type"], stats=all_pokemon[i]["Stats"]))
+            if all(x in pokemon_list[i]["Type"] for x in type) and len(pokemon_list[i]["Type"]) == 1:
+                sorted_pokemon[i] = {"Type":pokemon_list[i]["Type"], "Stats":pokemon_list[i]["Stats"]}
 
-        #prints all pokemon with the specified type regardless of other type
-        elif type in all_pokemon[i]["Type"]:
-                print("{pokemon}\n Type: {type}\n Stats: {stats}\n".format(pokemon=i, type=all_pokemon[i]["Type"], stats=all_pokemon[i]["Stats"]))
+        #adds all pokemon with the specified type combinations
+        elif len(type) == 2:
+            if all(x in pokemon_list[i]["Type"] for x in type) and len(pokemon_list[i]["Type"]) == 2:
+                sorted_pokemon[i] = {"Type":pokemon_list[i]["Type"], "Stats":pokemon_list[i]["Stats"]}
 
-#prints all pokemon with a specified stats category with a maximum value, minimum value or specific value
-def pokemon_with_stats(stat_type, val, min=False, max=False):
-    all_pokemon = get_all_pokemon_name_type_stats()
-    pokemon_stats_dict = {}
-    for i in all_pokemon:
+        #adds all pokemon with the specified type regardless of other type
+        elif all(x in pokemon_list[i]["Type"] for x in type):
+            sorted_pokemon[i] = {"Type":pokemon_list[i]["Type"], "Stats":pokemon_list[i]["Stats"]}
+    return sorted_pokemon
+
+#returns a dictionary of all the pokemon with a specified stats category with a maximum value, minimum value or specific value
+def pokemon_with_stats(stat_type, val, min=False, max=False, pokemon_list=pokedex):
+    
+    sorted_pokemon = {}
+    for i in pokemon_list:
         if min == True:
-            if all_pokemon[i]["Stats"][stat_type] >= val:
-                pokemon_stats_dict[i] = all_pokemon[i]["Stats"][stat_type]
+            if pokemon_list[i]["Stats"][stat_type] >= val:
+                sorted_pokemon[i] = {"Type":pokemon_list[i]["Type"], "Stats":pokemon_list[i]["Stats"]}
         elif max == True: 
-            if all_pokemon[i]["Stats"][stat_type] <= val:
-                pokemon_stats_dict[i] = all_pokemon[i]["Stats"][stat_type]
+            if pokemon_list[i]["Stats"][stat_type] <= val:
+                sorted_pokemon[i] = {"Type":pokemon_list[i]["Type"], "Stats":pokemon_list[i]["Stats"]}
         else:
-            if all_pokemon[i]["Stats"][stat_type] == val:
-                pokemon_stats_dict[i] = all_pokemon[i]["Stats"][stat_type]
+            if pokemon_list[i]["Stats"][stat_type] == val:
+                sorted_pokemon[i] = {"Type":pokemon_list[i]["Type"], "Stats":pokemon_list[i]["Stats"]}
 
-    pokemon_stats_dict = dict(sorted(pokemon_stats_dict.items(), key=lambda item: item[1]))
-    
-    for i in pokemon_stats_dict:
-        print("{pokemon}\n Type: {type}\n Stats: {stats}\n".format(pokemon=i, type=all_pokemon[i]["Type"], stats=all_pokemon[i]["Stats"]))
-    
+    sorted_pokemon = dict(sorted(sorted_pokemon.items(), key=lambda item: item[1]["Stats"][stat_type]))
+    return sorted_pokemon
 
-# pokemon_by_starting_letter("D")
-# pokemon_with_type("FIRE")
-pokemon_with_stats("Speed", 100, max=True)
-# display_all_pokemon()
-# print(get_all_pokemon_name_type_stats())
+    
+# pokemon_by_starting_letter("Partner")
+# pokemon_with_type("WATER", "ICE")
+# pokemon_with_stats("Speed", 100, max=True)
+# display_pokemon_list(pokemon_list=pokemon_with_type("WATER", pokemon_list=pokemon_with_stats("Total", 600, max=True)))
+# display_pokemon_list(pokemon_list=pokemon_with_type("WATER", pokemon_list=pokemon_by_starting_letter("A")))
+display_pokemon_list(pokemon_list=pokemon_with_type("GHOST", pokemon_list=pokemon_with_stats("Total", 600)))
