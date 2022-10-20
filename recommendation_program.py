4c5626405667c9b0b1d23ab7231df3f8eff3ad41
Danish Hilman <danishhilmanazizul@gmail.com>
2022-10-20 14:12:22 +0800

    Added Interface class and Menu class

--- recommendation_program.py
+++ recommendation_program.py
@@ -54,10 +54,95 @@ def pokemon_with_stats(stat_type, val, min=False, max=False, pokemon_list=pokede
     sorted_pokemon = dict(sorted(sorted_pokemon.items(), key=lambda item: item[1]["Stats"][stat_type]))
     return sorted_pokemon
 
+
+#interface class
+class Interface:
+    next_menu = None
+    def __init__(self):
+        self.text = "Enter 1 or 2"
+        self.choice = 0
+        self.current_menu = Interface
+
+    def logo(self):
+        print(""" .----------------. .----------------. .----------------. .----------------. .----------------. .----------------. .----------------. 
+| .--------------. | .--------------. | .--------------. | .--------------. | .--------------. | .--------------. | .--------------. |
+| |   ______     | | |     ____     | | |  ___  ____   | | |  _________   | | |  ________    | | |  _________   | | |  ____  ____  | |
+| |  |_   __ \   | | |   .'    `.   | | | |_  ||_  _|  | | | |_   ___  |  | | | |_   ___ `.  | | | |_   ___  |  | | | |_  _||_  _| | |
+| |    | |__) |  | | |  /  .--.  \  | | |   | |_/ /    | | |   | |_  \_|  | | |   | |   `. \ | | |   | |_  \_|  | | |   \ \  / /   | |
+| |    |  ___/   | | |  | |    | |  | | |   |  __'.    | | |   |  _|  _   | | |   | |    | | | | |   |  _|  _   | | |    > `' <    | |
+| |   _| |_      | | |  \  `--'  /  | | |  _| |  \ \_  | | |  _| |___/ |  | | |  _| |___.' / | | |  _| |___/ |  | | |  _/ /'`\ \_  | |
+| |  |_____|     | | |   `.____.'   | | | |____||____| | | | |_________|  | | | |________.'  | | | |_________|  | | | |____||____| | |
+| |              | | |              | | |              | | |              | | |              | | |              | | |              | |
+| '--------------' | '--------------' | '--------------' | '--------------' | '--------------' | '--------------' | '--------------' |
+ '----------------' '----------------' '----------------' '----------------' '----------------' '----------------' '----------------' """)
+
+    def prompt_box_upper(self):
+        print(" ", end="")
+        for i in range(len(self.text)+2):
+            print("_", end="")
+        else:
+            print("\n", end="")
+
+        print("|", end="")
+
+        for i in range(len(self.text)+2):
+            print(" ",end="")
+        else:
+            print("|")
     
+    def prompt_box_lower(self):
+        print("|", end="")
+        for i in range(len(self.text)+2):
+            print("_", end="")
+        else:
+            print("|")
+
+    def prompt_choice(self):
+        self.prompt_box_upper()
+        print("|", self.text, "|")
+        self.prompt_box_lower()
+        
+        print("\n", end="")
+        print("= ", end="")
+        self.choice = int(input())
+        
+    def option_1(self):
+        Interface.next_menu = Menu_1
+        Interface.next_menu.display_menu(Interface.next_menu)
+    
+
+#First Menu/Page
+class Menu_1:
+
+    def display_menu(self):
+        print("DISPLAY")
+        # display_pokemon_list()
+
+    def option_1(self):
+        print("OPTION1")
+
+    def option_2(self):
+        exit()
+    
+
+#Instantiate
+interface = Interface()
+interface.logo()
+
+#Interface Start
+while True:
+    if interface.choice == 1:
+        interface.current_menu.option_1(interface)
+        interface.current_menu = Interface.next_menu
+        interface.choice = None
+        
+
+    interface.prompt_choice()
+
+
 # pokemon_by_starting_letter("Partner")
 # pokemon_with_type("WATER", "ICE")
 # pokemon_with_stats("Speed", 100, max=True)
 # display_pokemon_list(pokemon_list=pokemon_with_type("WATER", pokemon_list=pokemon_with_stats("Total", 600, max=True)))
 # display_pokemon_list(pokemon_list=pokemon_with_type("WATER", pokemon_list=pokemon_by_starting_letter("A")))
-display_pokemon_list(pokemon_list=pokemon_with_type("GHOST", pokemon_list=pokemon_with_stats("Total", 600)))
+# display_pokemon_list(pokemon_list=pokemon_with_type("GHOST", pokemon_list=pokemon_with_stats("Total", 600)))
